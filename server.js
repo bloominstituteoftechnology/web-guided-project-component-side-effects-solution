c53ec6742b067de4a06afdb467b36934f681128e
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-13 14:35:21 +0200

    change api

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use((req, res, next) => {
 })
 
 const friends = [
-  { id: '1', name: 'Michael', email: 'michael@michael.com', age: 32, likes: ['hiking', 'science fiction', 'sightseeing'] },
-  { id: '2', name: 'Jeffrey', email: 'jeffrey@jeffrey.com', age: 28, likes: ['fishing', 'cooking', 'death metal'] },
-  { id: '3', name: 'Katrina', email: 'katrina@katrina.com', age: 25, likes: ['hiking', 'netflix', 'fishing'] },
-  { id: '4', name: 'Danielle', email: 'danielle@danielle.com', age: 22, likes: ['heavy metal', 'netflix', 'death metal'] },
-  { id: '5', name: 'Erik', email: 'erik@erik.com', age: 35, likes: ['reading', 'amazon', 'bird watching'] },
-  { id: '6', name: 'Justin', email: 'justin@justin.com', age: 24, likes: ['hiking', 'heavy metal', 'heavy rock'] },
+  { id: '1', name: 'Amber', email: 'amber@amber.com', age: 32, likes: ['hiking', 'science fiction', 'sightseeing'] },
+  { id: '2', name: 'Buddy', email: 'buddy@buddy.com', age: 28, likes: ['fishing', 'cooking', 'death metal'] },
+  { id: '3', name: 'Barbara', email: 'barbara@barbara.com', age: 25, likes: ['hiking', 'netflix', 'fishing'] },
+  { id: '4', name: 'Jonathan', email: 'jonathan@jonathan.com', age: 22, likes: ['heavy metal', 'netflix', 'death metal'] },
+  { id: '5', name: 'Marta', email: 'marta@marta.com', age: 35, likes: ['reading', 'amazon', 'bird watching'] },
+  { id: '6', name: 'Songa', email: 'songa@songa.com', age: 24, likes: ['hiking', 'heavy metal', 'heavy rock'] },
 ]
 
 app.get('/friends/:id', (req, res) => {
